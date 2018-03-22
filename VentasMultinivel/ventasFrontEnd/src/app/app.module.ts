a92eb0c255b68398437a72b6487dd020480de44e
BohorquezSD <none>
2018-03-22 14:22:39 -0500

    Creacion de servicios y enrutamiento

--- VentasMultinivel/ventasFrontEnd/src/app/app.module.ts
+++ VentasMultinivel/ventasFrontEnd/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
 import { RegistroClienteComponent } from './registro-cliente/registro-cliente.component';
 import { RegistroVentaComponent } from './registro-venta/registro-venta.component';
+
+import { ConexionOracleService } from './service/conexion-oracle.service';
 
 
 @NgModule({
@@ -14,9 +21,14 @@ import { RegistroVentaComponent } from './registro-venta/registro-venta.componen
     RegistroVentaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ConexionOracleService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ VentasMultinivel/ventasFrontEnd/src/app/service/conexion-oracle.service.ts
@@ -0,0 +1,194 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { HttpParams } from '@angular/common/http';
+import { log } from 'util';
+
+@Injectable()
+export class ConexionOracleService {  
+
+  headers = new Headers({
+    'Content-Type': 'application/x-www-form-urlencoded',
+  });
+
+  url: string = "http://localhost:3030/";
+  empleado: string = "empleado";
+  login: string = "login";
+  contactosCliente: string = "contacto";
+  cliente: string = "cliente";
+  auto: string = "auto";
+  autoCaracteristica: string = "autoCaracteristica";
+  autoParte: string = "autoParte";
+  valorAuto: string = "valorAuto";
+  parteLujo: string = "parteLujo";
+  maximo: string = "maximo";
+  postCotizar: string = "postCotizacion";
+  cotizacionPago: string = "cotizacionPago";
+  cotizacionCredito: string = "cotizacionCredito";
+  cotizacionSeparar: string = "cotizacionSeparar";
+  estadoCotizacion: string = "estadoCotizacion";
+  detallesCotizacion: string = "detallesCotizacion";
+  modalidadPago: string = "modalidadPago";
+  bancos: string = "bancos";
+  postAcuerdoPago: string = "postAcuerdoPago";
+  postEstado: string = "postCambiarEstado";
+  postSeparar: string = "postSepararAuto";
+  detalles30: string = "detallesPagoTreinta";
+  detalles70: string = "detallesPagoSetenta";
+  gruposFinancieros: string = "grupoFinanciero";
+  tiposTarjeta: string = "tipoTarjeta";
+  cotizacionAbono: string = 'cotizacionAbono';
+  abonarUpdate: string = 'postAbonarUpdate';
+  postPago: string = 'postPagar';
+
+  constructor(private http: Http) { }
+
+  addUserParams(parametros: any) {
+    if (parametros == null) {
+      parametros = {};
+    }
+    parametros['user'] = {};
+    parametros['pass'] = {};
+    parametros.user = localStorage.getItem('user');
+    parametros.pass = localStorage.getItem('pass');
+    return parametros;
+  }
+
+  oracleGet(tabla: string, parametros: any) {
+    return this.http.get(this.url + tabla, {
+      headers: this.headers,
+      //params: (parametros!=null)?new URLSearchParams(this.addUserParams(parametros)).toString():'',
+      params: new URLSearchParams(this.addUserParams(parametros)).toString(),
+    });
+    //}).toPromise();   
+  }
+
+  oraclePost(tabla, parametros) {
+    //let body = new URLSearchParams(parametros).toString();
+    return this.http.post(this.url + tabla, parametros, {
+      headers: this.headers,
+      params: new URLSearchParams(this.addUserParams(parametros)).toString()
+    });
+    //}).toPromise();
+  }
+
+  postPagar(parametro: any) {
+    return this.oraclePost(this.postPago, parametro);
+  }
+
+  postSepararAuto(parametro: any) {
+    return this.oraclePost(this.postSeparar, parametro);
+  }
+
+
+
+  postAbonarUpdate(parametro: any) {
+    return this.oraclePost(this.abonarUpdate, parametro);
+  }
+
+  postCambioEstado(parametro: any) {
+    return this.oraclePost(this.postEstado, parametro);
+  }
+
+  postAcuerdo(parametro: any) {
+    return this.oraclePost(this.postAcuerdoPago, parametro);
+  }
+
+  postCotizacion(parametro: any) {
+    return this.oraclePost(this.postCotizar, parametro);
+  }
+
+  getEmpleados() {
+    return this.oracleGet(this.empleado, null);
+  }
+
+  getCotizacionSeparar(id: number) {
+    return this.oracleGet(this.cotizacionSeparar, { idCliente: id });
+  }
+
+  getCotizacionPago(id: number) {
+    return this.oracleGet(this.cotizacionPago, { idCliente: id });
+  }
+
+  getCotizacionCredito(id: number) {
+    return this.oracleGet(this.cotizacionCredito, { idCliente: id });
+  }
+
+  getEmpleado(id: string) {
+    return this.oracleGet(this.empleado, { usuario: id });
+  }
+
+  getMaximo(tabla: string) {
+    return this.oracleGet(this.maximo, { tabla: tabla });
+  }
+
+  getAutoCaracteristica(id: number) {
+    return this.oracleGet(this.autoCaracteristica, { id: id });
+  }
+
+  getEstadoCotizacion(id: number) {
+    return this.oracleGet(this.estadoCotizacion, { id: id });
+  }
+
+  getDetallesPago30(id: number) {
+    return this.oracleGet(this.detalles30, { id: id });
+  }
+
+  getDetallesPago70(id: number) {
+    return this.oracleGet(this.detalles70, { id: id });
+  }
+
+  getDetallesCotizacion(id: number) {
+    return this.oracleGet(this.detallesCotizacion, { id: id });
+  }
+
+  getValorAuto(id: number) {
+    return this.oracleGet(this.valorAuto, { id: id });
+  }
+
+  getAutoParte(id: number) {
+    return this.oracleGet(this.autoParte, { id: id });
+  }
+
+  getTipoTarjeta() {
+    return this.oracleGet(this.tiposTarjeta, null);
+  }
+
+  getGrupoFinanciero() {
+    return this.oracleGet(this.gruposFinancieros, null);
+  }
+
+  getAuto() {
+    return this.oracleGet(this.auto, null);
+  }
+
+  getBancos() {
+    return this.oracleGet(this.bancos, null);
+  }
+
+  getModalidadPago() {
+    return this.oracleGet(this.modalidadPago, null);
+  }
+
+  getParteLujo() {
+    return this.oracleGet(this.parteLujo, null);
+  }
+
+  getContactossCliente(idCliente: number) {
+    return this.oracleGet(this.contactosCliente, { id: idCliente });
+  }
+
+  getCliente(idCliente: number) {
+    return this.oracleGet(this.cliente, { id: idCliente });
+  }
+
+  getCotizacionAbono(id: number) {
+    return this.oracleGet(this.cotizacionAbono, { idCliente: id });
+  }
+  getLogin(user: string, pass: string) {
+    localStorage.setItem('user', user);
+    localStorage.setItem('pass', pass);
+    return this.oracleGet("login", null);
+  }
+
+}
